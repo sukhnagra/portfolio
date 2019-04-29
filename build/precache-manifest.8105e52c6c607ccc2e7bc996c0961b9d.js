f77c604cb40b97ebc287fc876beceedeeb8e14f7
sukh.nagra <sukh.nagra@e2x.co.uk>
2019-04-29 23:41:30 +0100

    [SN] added build folder

---
+++ build/precache-manifest.8105e52c6c607ccc2e7bc996c0961b9d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e82c6fc29087284d681aeaaf88a3531",
+    "url": "/static/media/profile-pic.3e82c6fc.png"
+  },
+  {
+    "revision": "859c1d77babb720b73076b2e4d25eb19",
+    "url": "/static/media/night-background-2560x1600-desktop.859c1d77.jpg"
+  },
+  {
+    "revision": "2613899f7a47b8f43a5b0029dd13b6d3",
+    "url": "/static/media/aboutMeBackground.2613899f.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "713e5593f91571b74633",
+    "url": "/static/js/main.713e5593.chunk.js"
+  },
+  {
+    "revision": "7645649a8c7ca296eba7",
+    "url": "/static/js/1.7645649a.chunk.js"
+  },
+  {
+    "revision": "713e5593f91571b74633",
+    "url": "/static/css/main.44145b40.chunk.css"
+  },
+  {
+    "revision": "7645649a8c7ca296eba7",
+    "url": "/static/css/1.542bc7a0.chunk.css"
+  },
+  {
+    "revision": "666fbf4b5d8e1c19142aee5d81764434",
+    "url": "/index.html"
+  }
+];
